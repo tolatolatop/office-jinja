1a7584e3464651b29fa0166415a7f2aec6c92045
tolatolatop <tolatolatop@gmail.com>
2025-11-25 06:11:01 +0800

    feat: 支持检查图片替换信息

--- src/office_jinja/render_docx.py
+++ src/office_jinja/render_docx.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from docxtpl import DocxTemplate
 from .models import Context, Template
 
@@ -13,6 +14,16 @@ def render_all_docs_from_template(template: Template) -> None:
 def render_docx(template_file: str, context: Context) -> None:
     doc = DocxTemplate(template_file)
     doc.render(context.data)
-    for pic_file, pic_path in context.pic.items():
-        doc.replace_pic(pic_file, pic_path)
+    replace_all_pictures(doc, context)
     doc.save(template_file.replace(".docx", ".rendered.docx"))
+
+
+def replace_all_pictures(doc: DocxTemplate, context: Context) -> None:
+    for pic_file, pic_path in context.pic.items():
+        if not os.path.exists(pic_path):
+            logging.error(f"Picture file {pic_path} not found")
+            raise FileNotFoundError(f"Picture file {pic_path} not found")
+        if pic_file not in doc.get_pic_map():
+            logging.debug(f"Picture file {pic_file} not found")
+            continue
+        doc.replace_pic(pic_file, pic_path)

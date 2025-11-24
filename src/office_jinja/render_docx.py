2dadf7a207276b74ecd1dfd2bc362fb06f06eed2
tolatolatop <tolatolatop@gmail.com>
2025-11-25 06:11:22 +0800

    feat: 添加注释

--- src/office_jinja/render_docx.py
+++ src/office_jinja/render_docx.py
@@ -5,6 +5,9 @@ from .models import Context, Template
 
 
 def render_all_docs_from_template(template: Template) -> None:
+    """
+    渲染所有docx模板
+    """
     for template_file in template.templates:
         if not template_file.endswith(".docx"):
             continue
@@ -12,6 +15,9 @@ def render_all_docs_from_template(template: Template) -> None:
 
 
 def render_docx(template_file: str, context: Context) -> None:
+    """
+    渲染单个docx模板
+    """
     doc = DocxTemplate(template_file)
     doc.render(context.data)
     replace_all_pictures(doc, context)
@@ -19,6 +25,9 @@ def render_docx(template_file: str, context: Context) -> None:
 
 
 def replace_all_pictures(doc: DocxTemplate, context: Context) -> None:
+    """
+    替换所有图片
+    """
     for pic_file, pic_path in context.pic.items():
         if not os.path.exists(pic_path):
             logging.error(f"Picture file {pic_path} not found")

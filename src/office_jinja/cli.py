90415c7ff02bc5ea99783cfcd68f51e633e5ee37
tolatolatop <tolatolatop@gmail.com>
2025-11-30 13:31:18 +0800

    feat: 支持渲染表格

--- src/office_jinja/cli.py
+++ src/office_jinja/cli.py
@@ -3,6 +3,7 @@ from click import argument, command, version_option
 
 from office_jinja.models import Template
 from office_jinja.render_docx import render_all_docs_from_template
+from office_jinja.render_xlsx import render_all_xlsx_from_template
 
 version = "0.1.0"
 
@@ -11,9 +12,13 @@ version = "0.1.0"
 @version_option(version=version)
 @argument("config")
 def generate(config: str):
+    """
+    生成Office文档
+    """
     template = Template.model_validate_yaml(config)
     print(template)
     render_all_docs_from_template(template)
+    render_all_xlsx_from_template(template)
 
 
 if __name__ == "__main__":
